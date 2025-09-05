02b26279e57464d97a8c5a378d912bd63965d2aa
sz16 <143932346+sz16@users.noreply.github.com>
2025-09-05 21:19:04 +0700

    Add files via upload

--- task.py
+++ task.py
@@ -47,28 +47,8 @@ def setup_task(bot: "MyBot"):
             if delta == 14:
                 await chat(f"<@{id}> Hmmm, bro chưa hoạt động được 14 ngày rồi. Sớm quay lại server nhá")
                 bot.data.getWarn(id)
-            elif delta == 24:
-                await chat(f"<@{id}> Hmmm, bro chưa hoạt động gì được 24 ngày rồi. Bot sẽ xem xét việc kick các thành viên không hoạt động trong thời gian dài")
+            elif delta != 0 and delta % 24 == 0:
+                await chat(f"<@{id}> Chưa hoạt động gì được {delta} ngày rồi. Chúng tôi sẽ xem xét việc kick bạn")
                 bot.data.getWarn(id)
-            elif delta == 25:
-                #Kick user
-                logger.info(f"Kick user {id} for not active for 25 days")
-                member = server.get_member(int(id))
-                if member is None:
-                    logger.error("Member is None")
-                    continue
-                bot.data.getWarn(id)
-                try:
-                    await chat(f"<@{id}> Dựa vào lịch sử được ghi nhận bởi bot, chúng tôi nhận thấy bạn đã không hoạt động trong 25 ngày. Vì vậy, bot sẽ thực hiện kick người dùng để giúp server không bị loãng thành viên.")
-                    await member.kick(reason="Not active for 25 days")
-                    logger.info(f"Kick user {member} for not active for 25 days")
-                except discord.Forbidden:
-                    await chat(f"Có lỗi đã xảy ra. Bot không đủ quyền hạn để thực hiện kick user này.")
-                except Exception as e:
-                    logger.error(e)
 
-
-    check.start()    
-
-
-
+    check.start()
